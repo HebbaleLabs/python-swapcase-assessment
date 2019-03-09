2a83feb1898ed7516babdc44d2512d79743f2eb0
Vineet Reynolds Pereira <Vineet.Reynolds@gmail.com>
2019-03-10 04:15:47 +0530

    Converted to parameterized tests.

--- tests/test_swapcase.py
+++ tests/test_swapcase.py
@@ -1,49 +1,40 @@
 import unittest
-from delayed_assert import expect, assert_expectations
 from swapcase import swapcharactercase
+from parameterized import parameterized
 
 class SwapCaseTest(unittest.TestCase):
 
-  def test_swap_lowercase(self):
-    expect( lambda: self.assertEqual(swapcharactercase('abc'), 'ABC'))
-    assert_expectations()
+  @parameterized.expand([
+    ('short string with lowercase chars', 'abc', 'ABC'),
+    ('short string with uppercase chars', 'XYZ', 'xyz'),
+    ('short string with mixedcase chars', 'abcxyzABCXYZ', 'ABCXYZabcxyz')
+  ])
+  def test_swap_simple(self, name, input_value, expected):
+    self.longMessage = True
+    actual = swapcharactercase(input_value)
+    message = 'For input {0}, expected value = {1}, and actual value = {2}'.format(input_value, expected, actual)
+    self.assertEqual(expected, actual, message)
 
-  def test_swap_uppercase(self):
-    expect( lambda: self.assertEqual(swapcharactercase('XYZ'), 'xyz'))
-    assert_expectations()
+  @parameterized.expand([
+    ('empty string', '', ''),
+    ('string with spaces', 'The sky is clear', 'tHE SKY IS CLEAR'),
+    ('string with other chars', 'Chicago P.D.', 'cHICAGO p.d.')
+  ])
+  def test_swap_specialchars(self, name, input_value, expected):
+    self.longMessage = True
+    actual = swapcharactercase(input_value)
+    message = 'For input {0}, expected value = {1}, and actual value = {2}'.format(input_value, expected, actual)
+    self.assertEqual(expected, actual, message)
 
-  def test_swap_emptystring(self):
-    expect( lambda: self.assertEqual(swapcharactercase(''), ''))
-    assert_expectations()
-
-  def test_swap_mixedcase(self):
-    expect( lambda: self.assertEqual(swapcharactercase('abcxyzABCXYZ'), 'ABCXYZabcxyz'))
-    assert_expectations()
-
-  def test_swap_mixedcase_separated_by_space(self):
-    expect( lambda: self.assertEqual(swapcharactercase('The sky is clear'), 'tHE SKY IS CLEAR'))
-    assert_expectations()
-
-  def test_swap_mixedcase_separated_by_otherchars(self):
-    expect( lambda: self.assertEqual(swapcharactercase('Chicago P.D.'), 'cHICAGO p.d.'))
-    assert_expectations()
-
-  def test_swap_lowercase_longstring(self):
-    expect( lambda: self.assertEqual(swapcharactercase('pleasewaitoutsidethehouse'), 'PLEASEWAITOUTSIDETHEHOUSE'))
-    assert_expectations()
-
-  def test_swap_uppercase_longstring(self):
-    expect( lambda: self.assertEqual(swapcharactercase('ROCKMUSICAPPROACHESATHIGHVELOCITY'), 'rockmusicapproachesathighvelocity'))
-    assert_expectations()
-
-  def test_swap_mixedcase_longstring(self):
-    expect( lambda: self.assertEqual(swapcharactercase('EngInEErIngrEspOnsIbIlIty'), 'eNGiNeeRiNGReSPoNSiBiLiTY'))
-    assert_expectations()
-
-  def test_swap_mixedcase_with_seperators_longstring(self):
-    expect( lambda: self.assertEqual(swapcharactercase('It\'s Not All It\'s Cracked Up To Be'), 'iT\'S nOT aLL iT\'S cRACKED uP tO bE'))
-    assert_expectations()
-
-  def test_swap_alphanum(self):
-    expect( lambda: self.assertEqual(swapcharactercase('I often see the time 11:11 or 12:34 on The Big Ben.'), 'i OFTEN SEE THE TIME 11:11 OR 12:34 ON tHE bIG bEN.'))
-    assert_expectations()
+  @parameterized.expand([
+    ('long string with lowercase chars', 'pleasewaitoutsidethehouse', 'PLEASEWAITOUTSIDETHEHOUSE'),
+    ('long string with uppercase chars', 'ROCKMUSICAPPROACHESATHIGHVELOCITY', 'rockmusicapproachesathighvelocity'),
+    ('long string with mixedcase chars', 'EngInEErIngrEspOnsIbIlIty', 'eNGiNeeRiNGReSPoNSiBiLiTY'),
+    ('long string with mixedcase chars and separators', 'It\'s Not All It\'s Cracked Up To Be', 'iT\'S nOT aLL iT\'S cRACKED uP tO bE'),
+    ('long string with alphanum chars', 'I often see the time 11:11 or 12:34 on The Big Ben.', 'i OFTEN SEE THE TIME 11:11 OR 12:34 ON tHE bIG bEN.')
+  ])
+  def test_swap_perf(self, name, input_value, expected):
+    self.longMessage = True
+    actual = swapcharactercase(input_value)
+    message = 'For input {0}, expected value = {1}, and actual value = {2}'.format(input_value, expected, actual)
+    self.assertEqual(expected, actual, message)
