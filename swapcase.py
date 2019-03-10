98a89aac25f51317d64376d1777850d563c647b7
Vineet Reynolds Pereira <Vineet.Reynolds@gmail.com>
2019-03-10 08:33:42 +0530

    Added instructions and main method.

--- swapcase.py
+++ swapcase.py
@@ -1,3 +1,17 @@
 
 def swapcharactercase(str):
+  #
+  # IMPORTANT:
+  # 1. Write your solution in this function
+  # 2. Do not make changes to the function signature
+  # 3. Use the 'pip-install' command in the IDE, to install any requirements. This takes a few seconds.
+  # 4. Use the 'run' command in the IDE, to run the main method. You can also invoke the main method from the terminal.
+  # 5. Use the 'run-tests' command in the IDE, to run unit tests. The 'pip-install' command must be run before this.
   return None
+
+def main():
+  case_swapped_string = swapcharactercase('Hello World')
+  print('String after swapping case: {0}\n'.format(case_swapped_string))
+
+if __name__ == '__main__':
+  main()
